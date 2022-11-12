8db69c07b9282c2e0987ca79291d70fea729d8b1
Achraf Maghous <acmaghou@e2r9p13.1337.ma>
2022-11-12 12:50:31 +0100

    push to test in vm

--- srcs/requirements/mariadb/config/conf.sql
+++ srcs/requirements/mariadb/config/conf.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE if not exists my_database;
-CREATE USER 'acmaghou42'@'%';
-GRANT ALL PRIVILEGES ON my_database.* to 'acmaghou'@'%' IDENTIFIED BY '9998';
+CREATE USER 'achraf'@'%';
+GRANT ALL PRIVILEGES ON *.* to 'achraf'@'%' IDENTIFIED BY '9998';
 alter user 'root'@'localhost' identified by 'dream';
 FLUSH PRIVILEGES;
